23fb9f48e096bbad80dee326495b073c2d37c217
oscarotero <oom@oscarotero.com>
2018-01-14 12:46:23 +0100

    100% code coverage

--- tests/ProxyTest.php
+++ tests/ProxyTest.php
@@ -1,7 +1,9 @@
 <?php
+declare(strict_types = 1);
 
 namespace Middlewares\Tests;
 
+use GuzzleHttp\Client;
 use Middlewares\Proxy;
 use Middlewares\Utils\Dispatcher;
 use Middlewares\Utils\Factory;
@@ -11,14 +13,67 @@ class ProxyTest extends TestCase
 {
     public function testProxy()
     {
-        $request = Factory::createServerRequest([], 'GET', 'http://example.com/middlewares/psr15-middlewares');
-        $target = Factory::createUri('https://github.com');
-
-        $response = Dispatcher::run([
-            new Proxy($target),
-        ], $request);
+        $response = Dispatcher::run(
+            [
+                new Proxy(Factory::createUri('https://github.com')),
+            ],
+            Factory::createServerRequest([], 'GET', 'http://example.com/middlewares/psr15-middlewares')
+        );
 
         $html = (string) $response->getBody();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertNotFalse(stripos($html, 'middlewares/psr15-middlewares'));
+    }
+
+    public function testOptionsAndClient()
+    {
+        $received = false;
+        $response = Dispatcher::run(
+            [
+                (new Proxy(Factory::createUri('https://github.com')))
+                    ->client(new Client())
+                    ->options([
+                        'on_headers' => function () use (&$received) {
+                            $received = true;
+                        },
+                    ]),
+            ],
+            Factory::createServerRequest([], 'GET', 'http://example.com/middlewares/psr15-middlewares')
+        );
+
+        $html = (string) $response->getBody();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertNotFalse(stripos($html, 'middlewares/psr15-middlewares'));
+        $this->assertTrue($received);
+    }
+
+    public function testError()
+    {
+        $response = Dispatcher::run(
+            [
+                new Proxy(Factory::createUri('http://not-found.com')),
+            ],
+            Factory::createServerRequest([], 'GET', 'http://example.com/middlewares/psr15-middlewares')
+        );
+
+        $html = (string) $response->getBody();
+
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testTargetPath()
+    {
+        $response = Dispatcher::run(
+            [
+                new Proxy(Factory::createUri('https://github.com/middlewares')),
+            ],
+            Factory::createServerRequest([], 'GET', 'http://example.com/psr15-middlewares')
+        );
+
+        $html = (string) $response->getBody();
+
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertNotFalse(stripos($html, 'middlewares/psr15-middlewares'));
     }
